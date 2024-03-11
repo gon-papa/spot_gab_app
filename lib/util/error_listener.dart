449d6e9fe625c458067cf727e63ec2dce52df429
naoki_gonda <naoki.0821g@gmail.com>
2024-03-12 00:46:56 +0900

    エラーハンドリングとリフレッシュトークン取得を実装

--- lib/util/error_listener.dart
+++ lib/util/error_listener.dart
@@ -1,4 +1,4 @@
-import 'package:spot_gab_app/importer.dart';
+import 'package:spot_gab_app/importer.dart'; // このimport文は、あなたのプロジェクトに依存します。
 
 class ErrorListenerWidget extends ConsumerWidget {
   const ErrorListenerWidget({Key? key, required this.child}) : super(key: key);
@@ -7,38 +7,21 @@ class ErrorListenerWidget extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final _Providers = ref.watch(errorListnerProviders);
-    final message = ref.watch(_Providers.errorMessageProvider);
+    final message = ref.watch(errorMessageProvider);
+    final navigatorKey = ref.read(globalKeyProvider);
+
     if (message != null) {
       WidgetsBinding.instance.addPostFrameCallback((_) {
-        final nav = ref.watch(globalKeyProvider).currentState;
-        // Builderを使用して新しいBuildContextを生成し、それをshowErrorDialogに渡す
         showDialog(
-          context: nav!.overlay!.context, // showDialogを呼び出すためのcontext
+          context: navigatorKey.currentState?.context ?? context,
+          barrierDismissible: false,
           builder: (BuildContext dialogContext) {
-            // showDialogのbuilderが提供する新しいcontext
-            return AlertDialog(
-              backgroundColor: Colors.white,
-              title: Text(L10n.of(dialogContext)?.error ?? ''),
-              content: Text(
-                message,
-                style: TextStyle(fontSize: 16.sp),
-              ),
-              actions: [
-                TextButton(
-                  child: Text(
-                    'OK',
-                    style: TextStyle(fontSize: 16.sp),
-                  ),
-                  onPressed: () {
-                    // エラーメッセージをクリア
-                    ref.read(_Providers.errorMessageProvider.notifier).state =
-                        null;
-                    // dialogContextを使用してダイアログを閉じる
-                    Navigator.of(dialogContext).pop();
-                  },
-                ),
-              ],
+            return Dialog(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0)),
+              elevation: 0,
+              backgroundColor: Colors.transparent,
+              child: _buildDialogContent(dialogContext, ref, message),
             );
           },
         );
@@ -47,16 +30,75 @@ class ErrorListenerWidget extends ConsumerWidget {
 
     return child;
   }
-}
 
-class ErrorListnerProviders {
-  final defaultError = 'defaultError';
-  final errorMessageProvider = StateProvider<String?>((ref) => null);
-
-  void notifyError(ref, String message) {
-    ref.read(errorMessageProvider.notifier).state = message;
+  Widget _buildDialogContent(
+      BuildContext context, WidgetRef ref, String message) {
+    return Container(
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 8.h),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(Icons.error_outline, color: Colors.red, size: 24),
+              SizedBox(
+                width: 8,
+              ),
+              Text(
+                L10n.of(context)?.error ?? '',
+                style: TextStyle(fontSize: 16.sp, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+          SizedBox(height: 16.h),
+          Text(
+            message,
+            style: TextStyle(fontSize: 16.sp),
+          ),
+          SizedBox(height: 24.h),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: TextButton(
+              child: Text('OK', style: TextStyle(fontSize: 16.sp)),
+              onPressed: () {
+                if (ref.read(_expired_authentication.notifier).state) {
+                  ref.read(errorMessageProvider.notifier).state = null;
+                  ref.read(_expired_authentication.notifier).state = false;
+                  SignInRoute().go(context);
+                } else {
+                  ref.read(errorMessageProvider.notifier).state = null;
+                  Navigator.of(context).pop();
+                }
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
 
-final errorListnerProviders =
-    Provider.autoDispose((ref) => ErrorListnerProviders());
+final errorMessageProvider = StateProvider<String?>((ref) => null);
+final _expired_authentication = StateProvider<bool>((ref) => false);
+final errorMessageHandle = Provider((ref) => (
+      String? message,
+      BuildContext? context,
+    ) {
+      if (context != null && message == DEFAULT_ERROR) {
+        message = L10n.of(context)?.errorHasOccurred ?? '';
+      } else if (context != null && message == EXPIRED_AUTHENTICATION) {
+        message = L10n.of(context)?.expiredAuthentication ?? '';
+        ref.read(_expired_authentication.notifier).state = true;
+      }
+      ref.read(errorMessageProvider.notifier).state = message;
+    });
+
+const DEFAULT_ERROR = 'defaulrError';
+const EXPIRED_AUTHENTICATION = 'expiredAuthentication';
